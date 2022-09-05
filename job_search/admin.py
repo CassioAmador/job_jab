250f8c8c8e3dc1535ea319255249270098b91439
CassioAmador <cassioamador@utfpr.edu.br>
2022-09-05 00:42:36 -0300

    Delete job_search directory

--- job_search/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Job
-
-class JobAdmin(admin.ModelAdmin):
-    list_display = ("doc_id", "employer_name", "date_posted", "employment_type", "location", "title", "description")
-
-admin.site.register(Job, JobAdmin)
--- job_search/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.views.generic import ListView
-
-from . import models
-
-class SearchResultsView(ListView):
-    model = models.Job
-    template_name = 'job_search/search_results.html'
